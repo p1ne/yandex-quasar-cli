b498dc12562c1bc22c4f1fc20c399ec1f9323c1a
Nikita Nalyutin <nikita.nalyutin@servpoint.org>
2024-02-20 13:28:42 +0300

    Updated deps

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='yandex-quasar-cli',
-    version='0.1.1',
+    version='0.1.2',
     packages=['yandex_quasar_cli', 'yandex_quasar_cli.quasar_types'],
     url='https://github.com/p1ne/yandex-quasar-cli',
     license='Apache-2.0',
